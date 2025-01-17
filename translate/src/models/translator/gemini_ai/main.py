690883905f92f556f31368bd4fc333b4a221391a
PranayPant <ppantg1@gmail.com>
2025-01-16 18:32:39 -0600

    add python formatting config

--- translate/src/models/translator/gemini_ai/main.py
+++ translate/src/models/translator/gemini_ai/main.py
@@ -12,19 +12,22 @@ class GeminiAI(AIModel):
     async def translate(self, transcript_id: str, include_sentences: bool, include_transcript: bool, include_srt: bool) -> TranscriptRecord:
         transcript_record = await get_transcription(transcript_id=transcript_id, include_sentences=True, include_transcript=False, include_srt=False)
         sentences = transcript_record.sentences
+        translated_sentences = self.translate_sentences(sentences)
+        transcript_record.sentences = translated_sentences
+        return transcript_record
 
     def translate_sentences(self, sentences: list[SubtitleRecord]) -> list[SubtitleRecord]:
 
         model = genai.GenerativeModel(self.name)
         prompt = """
-    You are given a timestamped Hindi transcript in the form of an array.
-    Translate the Hindi transcript to English, ignoring any Sanskrit quotations.
+            You are given a timestamped Hindi transcript in the form of an array.
+            Translate the Hindi transcript to English, ignoring any Sanskrit quotations.
 
-    Use this JSON schema:
+            Use this JSON schema:
 
-    SubtitleRecord = {'text': str, 'start': int, 'end': int}
-    Return: list[SubtitleRecord]
-    """
+            SubtitleRecord = {'text': str, 'start': int, 'end': int}
+            Return: list[SubtitleRecord]
+            """
         result = model.generate_content(prompt)
         print(result)
         return result
